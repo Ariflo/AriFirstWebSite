b0c91c33c63d879531cc332df987e6e55aeb7bc6
AriFlo <ariflo@arianflores.com>
2015-12-09 16:47:42 -0800

    fixed server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // load the things we need
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -23,5 +24,5 @@ app.get('/index2.ejs', function(req, res) {
     res.render('pages/index2');
 });
 
-app.listen(8080);
+app.listen(port);
 console.log('8080 is the magic port');
